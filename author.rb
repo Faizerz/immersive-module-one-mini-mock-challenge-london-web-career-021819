c0c06ace411c2b8755dd5d25d4e904c4c971b984
github username <github email address>
2019-02-25 17:02:50 +0000

    Done.

--- author.rb
+++ author.rb
@@ -1,1 +1,43 @@
 
+class Author
+
+  attr_reader :author
+
+  @@all = []
+
+  def initialize(author)
+    @author = author
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def write_book(title, word_count)
+    Book.new(self, title, word_count)
+  end
+
+  def total_words
+    count = 0
+    Book.all.select do |book|
+      if book.author == self
+        count += book.word_count
+      end
+    end
+    count
+  end
+
+  def self.most_words
+    most_words = 0
+    most_author = nil
+    Book.all.each do |book|
+      if book.word_count > most_words
+        most_words = book.word_count
+        most_author = book.author
+      end
+    end
+    most_author
+  end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,16 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+fez = Author.new('Fez')
+khang = Author.new('Khang')
+
+book1 = Book.new(fez, 'Yo Mama', 100)
+book1 = Book.new(fez, 'Digme', 200)
+book1 = Book.new(fez, 'Poppy', 300)
+book1 = Book.new(khang, 'Juplo', 400)
+book1 = Book.new(khang, 'Plagerised', 500)
+
+
+
 binding.pry
-
-
+"yo"
